264a89584f4bd4b4227ee3f3e99f0d1330e03144
SRCinVA <scitarella@hotmail.com>
2020-12-06 05:06:41 -0500

    Began run-through on a quantum machine

--- qiskit_using_python.py
+++ qiskit_using_python.py
@@ -19,4 +19,26 @@ circuit.measure([0,1], [0, 1])      # remember that measuring causes collapse in
                                     # the parameters specify how the qubit register will map to the calssical register. 
                                     # Question: not a good explanation.  
 
-circuit.draw()                      # Amazingy, this will return an ASCII representation of the circuit. 
+circuit.draw()                      # Amazingly, this will return an ASCII representation of the circuit.  
+
+circuit.draw(output='mpl')          # This will render a MatplotLib version of the circuit. 
+
+# to run this code on a quantum machine:
+
+from qiskit import IBMQ
+
+IBM.save_account("whatever your token is")  #it will save your token
+IBM.load_account()
+provider = IBM.get_provider("ibm q")
+for backend in provider.backends():
+    try:
+        qubit_count = len(backend.properties().qubits)
+    except:
+        qubit_count = 'simulated'
+    print(f"{backend.name()} has {backend.status().pending_jobs} queued and {qubit_count} qubits")
+
+from qiskit.tools.monitor import job_monitor
+
+backend = provider.get_backend("ibmq_xyz")
+job = q.execute(circuit, backend=backend, shots = 500)  # 'shots' specifies how many times you will run it to get a useful representation.) 
+job_monitor(job)
